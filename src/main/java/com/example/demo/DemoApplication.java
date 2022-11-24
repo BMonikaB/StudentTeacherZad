f817e1d0af17d4ab6655d8f5bb17c7107eb9c2c5
BMonikaB <boreckamonika39@gmail.com>
2022-11-24 10:28:22 +0100

    last commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -50,25 +50,6 @@ public class DemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		//Testowanie, potrzebne było mi do bazy h2
-
-		Student student1 = new Student("Anna","Nowak",30,"annanowak@gmai.com","Matematyka");
-		Student student2 = new Student("Zuznanna","Nowak",40,"zosianowak@gmai.com","Polski");
-		Student student3 = new Student("ALa","Kot",35,"alakot@gmai.com","Matematyka");
-		Student student4 = new Student("MArtyna","Nowak",40,"zosianowak@gmai.com","Polski");
-
-		Teacher teacher1 = new Teacher("Matylda","Kowalska",70,"kowalskam@gmail.com","Matematyka");
-
-		teacherService.addTeacher(teacher1);
-		studentService.addStudent(student1);
-		studentService.addStudent(student2);
-		studentService.addStudent(student3);
-		studentService.addStudent(student4);
-
-
-		studentTeacherService.addStudentToTeacher(student1,teacher1);
-		studentTeacherService.addStudentToTeacher(student2,teacher1);
-		studentTeacherService.removeStudentFromTeacher(student1,teacher1);
 
 
 	}
